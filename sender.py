c6e25d5a85a380b222b91c233b27d47784952c12
Kotyara <krivonos-artem-alexandrovich@yandex.ru>
2017-03-24 23:00:29 +0300

    fix: add BARRIER to sender

--- sender.py
+++ sender.py
@@ -5,6 +5,8 @@ import pika
 
 import db
 
+BARRIER = 500
+
 
 def main():
     connection = pika.BlockingConnection(
@@ -18,8 +20,8 @@ def main():
         cursor = db_.cursor()
         now = datetime.now().timestamp()
         hosts = cursor.execute(
-            'SELECT host FROM records WHERE abs(last_refresh - ?) > 5',
-            (now,)
+            'SELECT host FROM records WHERE abs(last_refresh - ?) > ?',
+            (now, BARRIER)
         )
         for host in hosts:
             channel.basic_publish(exchange='logs', routing_key='', body=host[0])
